cfbfb93143c898951e61777a4ebeb371f35dfe38
Pete Lewis <pete.lewis@sparkfun.local>
2020-03-12 12:14:57 -0600

    added connected attribute and setter via peroperty()

--- qwiic_icm20948.py
+++ qwiic_icm20948.py
@@ -296,6 +296,8 @@ class QwiicIcm20948(object):
 		"""
 		return qwiic_i2c.isDeviceConnected(self.address)
 
+    connected = property(is_connected)
+	
 	# ----------------------------------
 	# begin()
 	#
